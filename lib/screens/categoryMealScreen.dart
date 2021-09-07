e08abe37d7f584f28692e063c3940bb7622f0bcc
karim <karimmokhtar71@gmail.com>
2021-09-08 00:51:54 +0100

    full Meelscreen

--- lib/screens/categoryMealScreen.dart
+++ lib/screens/categoryMealScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mealapp/dummy_data.dart';
+import 'package:mealapp/widget/meal.dart';
 //import 'package:mealapp/widget/categorie.dart';
 
 class CategoryMealScreen extends StatefulWidget {
@@ -14,14 +16,28 @@ class _MealScreen extends State<CategoryMealScreen> {
     // we recive the arguments
     final routeArg =
         ModalRoute.of(context)!.settings.arguments as Map<String?, String?>;
-    //final categoryID = routeArg["id"];
+    final categoryID = routeArg["id"];
     final categoryTitle = routeArg["title"];
-
+    // we filtre the liste of Meal and return the Meal corespand of the category id
+    final categorieMeal = DUMMY_MEALS.where((element) {
+      return element.categories!.contains(categoryID);
+    }).toList();
+    //  print(categorieMeal.length);
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle.toString()),
       ),
-      body: null,
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return MealItem(
+              imageUrl: categorieMeal[index].imageUrl,
+              title: categorieMeal[index].title,
+              duration: categorieMeal[index].duration,
+              complexity: categorieMeal[index].complexity,
+              affordability: categorieMeal[index].affordability);
+        },
+        itemCount: categorieMeal.length,
+      ),
     );
   }
 }
---
+++ lib/widget/meal.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:mealapp/module/meal.dart';
+
+class MealItem extends StatelessWidget {
+  final String? imageUrl;
+  final String? title;
+  final int? duration;
+  final Complexity? complexity;
+  final Affordability? affordability;
+
+  const MealItem(
+      {@required this.imageUrl,
+      @required this.title,
+      @required this.duration,
+      @required this.complexity,
+      @required this.affordability});
+  // to convert the enum to String text
+  String get complxityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+
+      case Complexity.Challenging:
+        return 'Challenging';
+
+      case Complexity.Hard:
+        return 'Hard';
+
+      default:
+        return 'Enknow';
+    }
+  }
+
+  //-------------------------------
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+
+      case Affordability.Luxurious:
+        return 'Luxurious';
+
+      case Affordability.Pricey:
+        return 'Pricey';
+
+      default:
+        return 'Enknow';
+    }
+  }
+
+  Widget? mealImg() {
+    if (imageUrl != null)
+      return Image.network(
+        this.imageUrl.toString(),
+        height: 200,
+        width: double.infinity,
+        fit: BoxFit.cover,
+      );
+    else
+      return CircularProgressIndicator(
+        color: Colors.black,
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(complexity.toString());
+    return InkWell(
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        elevation: 4,
+        margin: EdgeInsets.all(14),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                // to cut the image as the card reduis top
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15)),
+                  child: mealImg(),
+                ),
+                Positioned(
+                  bottom: 15,
+                  right: 2,
+                  child: Container(
+                    padding: EdgeInsets.only(top: 5, bottom: 5, left: 3),
+                    color: Colors.black.withOpacity(.6),
+                    width: 250,
+                    child: Text(
+                      title.toString(),
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(width: 6),
+                      Text(
+                        duration.toString() + 'min ',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                  Row(children: [
+                    Icon(Icons.work),
+                    SizedBox(width: 6),
+                    Text(
+                      complxityText, // from the get methode 
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ]),
+                  Row(children: [
+                    Icon(Icons.attach_money),
+                    SizedBox(width: 6),
+                    Text(affordabilityText,style: TextStyle(color: Colors.black),
+                    ) ,
+                  ]),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
